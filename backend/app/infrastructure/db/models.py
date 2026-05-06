9275ab4347e1b38ea60dcb734c399a2c763c92c1
Gabriel Vitor <gabriel.vitor03@gmail.com>
2026-05-06 18:51:49 -0300

    feat(backend): definir modelo de usuário utilizando SQLAlchemy ORM
    - Cria a classe Table_Base mapeada para a tabela 'user'.
    - Define as colunas id (PK), nome e email.
    - Utiliza a classe Base do projeto para integração com o motor de banco de dados.

---
+++ backend/app/infrastructure/db/models.py
@@ -0,0 +1,9 @@
+from app.database import Base
+from sqlalchemy import Column, Integer, String
+
+class Table_Base(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String)
+    email = Column(String)
