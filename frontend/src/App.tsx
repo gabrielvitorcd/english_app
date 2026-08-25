c937d8fd887123204b8b53dac29d5c36ecda80ae
Gabriel Vitor <gabriel.vitor03@gmail.com>
2026-08-25 17:30:45 -0300

    feat(geral): registra rota /transcode-test e envolve app com PlayerSessionProvider
    - importa TranscodeTestPage e adiciona rota /transcode-test
    - envolve BrowserRouter com PlayerSessionProvider para compartilhar sessão do player
    - prepara o app para o novo fluxo de sessão via IndexedDB

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import { ProgressoPage } from "./pages/Progresso/Progresso";
 import { ShadowingPage } from "./pages/Shadowing/Shadowing";
 import { HomePage } from "./pages/Select/SelectPath";
 import { HomePage as Home } from "./pages/Home/HomePage";
+import { TranscodeTestPage } from "./pages/TranscodeTest/TranscodeTest";
+import { PlayerSessionProvider } from "./contexts/PlayerSessionContext";
 
 import "./App.css";
 
@@ -12,16 +14,19 @@ export default function App() {
   return (
     <>
       <div className="app">
-        <BrowserRouter>
-          <Heading />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/selectpath" element={<HomePage />} />
-            <Route path="/player" element={<PlayerPage />} />
-            <Route path="/progresso" element={<ProgressoPage />} />
-            <Route path="/shadowing" element={<ShadowingPage />} />
-          </Routes>
-        </BrowserRouter>
+        <PlayerSessionProvider>
+          <BrowserRouter>
+            <Heading />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/selectpath" element={<HomePage />} />
+              <Route path="/player" element={<PlayerPage />} />
+              <Route path="/progresso" element={<ProgressoPage />} />
+              <Route path="/shadowing" element={<ShadowingPage />} />
+              <Route path="/transcode-test" element={<TranscodeTestPage />} />
+            </Routes>
+          </BrowserRouter>
+        </PlayerSessionProvider>
       </div>
     </>
   );
